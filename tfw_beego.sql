210e4c42e609c7808da064bfcf98318c88bc9e17
TuFengWei <2844270738@qq.com>
2020-06-30 13:17:00 +0800

    数据库

---
+++ tfw_beego.sql
@@ -0,0 +1,85 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 8.0.20 : Database - tfw_beego
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tfw_beego` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `tfw_beego`;
+
+/*Table structure for table `article` */
+
+DROP TABLE IF EXISTS `article`;
+
+CREATE TABLE `article` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) NOT NULL DEFAULT '',
+  `content` varchar(200) NOT NULL DEFAULT '',
+  `img` varchar(50) NOT NULL DEFAULT '',
+  `time` datetime NOT NULL,
+  `count` int NOT NULL DEFAULT '0',
+  `article_type_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `article` */
+
+insert  into `article`(`id`,`title`,`content`,`img`,`time`,`count`,`article_type_id`) values (30,'111','111','../static/img/2020-06-25 17-09-16.png','2020-06-25 09:09:16',0,9),(31,'222','222','../static/img/2020-06-25 17-09-28.png','2020-06-25 09:09:28',0,10),(32,'333','333','../static/img/2020-06-25 17-09-39.png','2020-06-25 09:09:39',0,11);
+
+/*Table structure for table `article_type` */
+
+DROP TABLE IF EXISTS `article_type`;
+
+CREATE TABLE `article_type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `article_type` */
+
+insert  into `article_type`(`id`,`type_name`) values (9,'体育新闻'),(10,'财经新闻'),(11,'教育新闻');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`password`) values (1,'111','111'),(3,'222','222');
+
+/*Table structure for table `user_articles` */
+
+DROP TABLE IF EXISTS `user_articles`;
+
+CREATE TABLE `user_articles` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `article_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `user_articles` */
+
+insert  into `user_articles`(`id`,`user_id`,`article_id`) values (1,1,16),(2,1,25),(3,1,16),(4,1,25),(5,1,25),(6,1,25),(7,1,25),(8,1,25),(9,1,25),(10,1,23),(11,1,25),(12,1,25),(13,1,22),(14,1,25),(15,3,25),(16,3,25);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
